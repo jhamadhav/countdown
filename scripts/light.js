d5d78d31b843adbe2136d6bc9d5ec690a1125b06
jhamadhav <jhamadhav28@gmail.com>
2022-12-28 01:18:33 +0530

    torch added

--- scripts/light.js
+++ scripts/light.js
@@ -8,10 +8,11 @@ document.addEventListener("mousemove", (e) => {
 
     let diff = Math.abs(cx - x)
     // console.log(diff);
-    let deg = Math.floor(100 * (diff / cx))
+    let deg = Math.floor(40 * (diff / cx))
     // console.log(deg);
     if (x < cx) {
-        deg = -Math.floor(40 * (diff / cx))
+        deg = -deg
     }
     document.getElementById("light").style.transform = `rotate(${deg}deg)`
+    document.getElementById("torch").style.transform = `rotate(${deg}deg)`
 })
