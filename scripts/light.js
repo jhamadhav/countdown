511d301732c15c4b1bc304c9b8ccfe91e1c3009f
jhamadhav <jhamadhav28@gmail.com>
2022-12-28 14:58:20 +0530

    torch to hand change

--- scripts/light.js
+++ scripts/light.js
@@ -8,11 +8,12 @@ document.addEventListener("mousemove", (e) => {
 
     let diff = Math.abs(cx - x)
     // console.log(diff);
-    let deg = Math.floor(40 * (diff / cx))
+    let deg = Math.floor(20 * (diff / cx))
     // console.log(deg);
     if (x < cx) {
-        deg = -deg
+        deg *= -1
+
     }
     document.getElementById("light").style.transform = `rotate(${deg}deg)`
-    document.getElementById("torch").style.transform = `rotate(${deg}deg)`
+    document.getElementById("torch").style.transform = `rotate(${deg * 0.3}deg)`
 })
